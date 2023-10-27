fdc2bf59b254acf81df51e141dde31645c66d73e
unknown <michaelgball7@gmail.com>
2023-10-27 13:35:25 -0600

    balanced data analysis

--- AmazonBalancedAnalysis.R
+++ AmazonBalancedAnalysis.R
@@ -2,34 +2,33 @@ library(tidyverse)
 library(vroom)
 library(tidymodels)
 library(embed)
-library(ggmosaic)
+
 
 #Read in data
 amazonTrain <- vroom("./train.csv")
 amazonTest <- vroom("./test.csv")
 
-##EDA
-ggplot(amazonTrain, aes(RESOURCE, ACTION))+geom_point()
-ggplot(amazonTrain, aes(MGR_ID, ACTION))+geom_point()
-
 amazonTrain$ACTION <- as.factor(amazonTrain$ACTION)
 
-my_recipe <- recipe(ACTION~., data=amazonTrain) %>%
-  step_mutate_at(all_numeric_predictors(), fn = factor) %>%# turn all numeric features into factors
-  step_other(all_nominal_predictors(), threshold = .01) %>% # combines categorical values that occur <5% into an "other" value
-  step_dummy(all_nominal_predictors()) 
-prep <- prep(my_recipe)
-baked <- bake
+library(themis)
 
+#Recipe
+my_recipe <- recipe(ACTION~., amazonTrain) %>%
+  step_mutate_at(all_numeric_predictors(), fn = factor) %>% # turn all numeric features into factors5
+  #step_other(all_nominal_predictors(), threshold = .001) %>% # combines categorical values that occur <5% into an "other" value
+  step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION)) %>%
+  step_normalize(all_nominal_predictors()) %>%
+  step_smote(all_outcomes(), neighbors=5)
+prep <- prep(my_recipe)
 
 ##Logistic Regression
 logit_mod <- logistic_reg() %>% #Type of model
   set_engine("glm")
 
 logit_wf <- workflow() %>%
-add_recipe(my_recipe) %>%
-add_model(logit_mod) %>%
-fit(data = amazonTrain)
+  add_recipe(my_recipe) %>%
+  add_model(logit_mod) %>%
+  fit(data = amazonTrain)
 
 logit_preds <- predict(logit_wf,new_data=amazonTest,type="prob")%>%
   bind_cols(., amazonTest) %>%
@@ -38,26 +37,16 @@ logit_preds <- predict(logit_wf,new_data=amazonTest,type="prob")%>%
   rename(Id=id)
 
 ## Write prediction file to CSV
-vroom_write(x=logit_preds, file="./LogitPreds.csv", delim=",") 
-
+vroom_write(x=logit_preds, file="./LogitPreds.csv", delim=",")
 
 ##Penalized Logistic Regression
-library(themis)
-
-my_recipe <- recipe(ACTION~., amazonTrain) %>%
-  step_mutate_at(all_numeric_predictors(), fn = factor) %>% # turn all numeric features into factors5
-  step_other(all_nominal_predictors(), threshold = .001) %>% # combines categorical values that occur <5% into an "other" value
-  step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION)) %>%
-  step_normalize(all_nominal_predictors()) %>%
-  step_smote(all_outcomes(), neighbors=5)
-prep <- prep(my_recipe)
 
 plogit_mod <- logistic_reg(mixture=tune(), penalty=tune()) %>% #Type of model
   set_engine("glmnet")
 
 plogit_wf <- workflow() %>%
-add_recipe(my_recipe) %>%
-add_model(plogit_mod)
+  add_recipe(my_recipe) %>%
+  add_model(plogit_mod)
 
 ## Grid of values to tune over
 tuning_grid <- grid_regular(penalty(),mixture(),levels = 4) 
@@ -67,23 +56,20 @@ folds <- vfold_cv(amazonTrain, v =5, repeats=1)
 
 ## Run the CV
 CV_results <- plogit_wf %>%
-tune_grid(resamples=folds,grid=tuning_grid,
-          metrics=metric_set(roc_auc)) #Or leave metrics NULL
+  tune_grid(resamples=folds,grid=tuning_grid,
+            metrics=metric_set(roc_auc)) #Or leave metrics NULL
 
 bestTune <- CV_results %>%
-select_best("roc_auc")
+  select_best("roc_auc")
 
 ## Finalize the Workflow & fit it
-plogit_mod <- logistic_reg(mixture=0, penalty=.0000000001) %>% #Type of model
-  set_engine("glmnet")
+final_wf <- plogit_wf %>%
+  finalize_workflow(bestTune) %>%
+  fit(data=amazonTrain)
 
-plogit_wf <- workflow() %>%
-  add_recipe(my_recipe) %>%
-  add_model(plogit_mod) %>%
-  fit(amazonTrain)
 
 ## Predict
-plogit_preds <- predict(plogit_wf,new_data=amazonTest,type="prob")%>%
+plogit_preds <- predict(final_wf,new_data=amazonTest,type="prob")%>%
   bind_cols(., amazonTest) %>%
   select(id, .pred_1) %>%
   rename(Action=.pred_1) %>%
@@ -92,12 +78,13 @@ plogit_preds <- predict(plogit_wf,new_data=amazonTest,type="prob")%>%
 ## Write prediction file to CSV
 vroom_write(x=plogit_preds, file="./PLogitPreds.csv", delim=",") 
 
+
 ##Random Forest
 rf_mod <- rand_forest(mtry = tune(),
                       min_n=tune(),
                       trees=500) %>%
-set_engine("ranger") %>%
-set_mode("classification")
+  set_engine("ranger") %>%
+  set_mode("classification")
 #params are 10 and 21
 
 rf_wf <- workflow() %>%
@@ -108,12 +95,12 @@ rf_tuning_grid <- grid_regular(mtry(c(1, 10)), min_n(), levels = 3)
 folds <- vfold_cv(amazonTrain, v = 5, repeats=1)
 
 CV_results <- rf_wf %>%
-tune_grid(resamples=folds,grid=rf_tuning_grid,metrics=metric_set(roc_auc)) #Or leave metrics NULL
+  tune_grid(resamples=folds,grid=rf_tuning_grid,metrics=metric_set(roc_auc)) #Or leave metrics NULL
 
 
 ## Find Best Tuning Parameters
 bestTune <- CV_results %>%
-select_best("roc_auc")
+  select_best("roc_auc")
 
 final_wf <- rf_wf %>%
   finalize_workflow(bestTune) %>%
@@ -127,7 +114,6 @@ rf_preds <- predict(final_wf, new_data=amazonTest, type="prob") %>%
 
 vroom_write(x=rf_preds, file="./RFPreds.csv", delim=",") 
 
-
 ##Naive Bayes
 library(tidymodels)
 library(discrim)
@@ -135,12 +121,12 @@ library(naivebayes)
 
 ## nb model
 nb_model <- naive_Bayes(Laplace=tune(), smoothness=tune()) %>%
-set_mode("classification") %>%
-set_engine("naivebayes") 
+  set_mode("classification") %>%
+  set_engine("naivebayes") 
 
 nb_wf <- workflow() %>%
-add_recipe(my_recipe) %>%
-add_model(nb_model) 
+  add_recipe(my_recipe) %>%
+  add_model(nb_model) 
 
 ## Tune smoothness and Laplace here
 tuning_grid <- grid_regular(Laplace(),smoothness(),levels = 4)
@@ -156,8 +142,8 @@ bestTune <- CV_results %>%
   select_best("roc_auc")
 
 final_wf <-nb_wf %>%
-finalize_workflow(bestTune) %>%
-fit(data=amazonTrain)
+  finalize_workflow(bestTune) %>%
+  fit(data=amazonTrain)
 
 ## Predict
 nb_preds <- predict(final_wf, new_data=amazonTest, type="prob") %>%
@@ -166,24 +152,17 @@ nb_preds <- predict(final_wf, new_data=amazonTest, type="prob") %>%
   rename(Action=.pred_1) %>%
   rename(Id=id)
 
-vroom_write(x=nb_preds, file="./NB_Preds.csv", delim=",") 
+vroom_write(x=nb_preds, file="./NB_Preds.csv", delim=",")
 
-
-##K distance
+##KNN
 library(kknn)
-library(doParallel)
-
-parallel::detectCores()
-cl <- makePSOCKcluster(3)
-registerDoParallel(cl)
-
 knn_model <- nearest_neighbor(neighbors=tune()) %>% 
   set_mode("classification") %>%
-set_engine("kknn")
+  set_engine("kknn")
 
 knn_wf <- workflow() %>%
-add_recipe(my_recipe) %>%
-add_model(knn_model)
+  add_recipe(my_recipe) %>%
+  add_model(knn_model)
 
 ## Tune smoothness and Laplace here
 tuning_grid <- grid_regular(neighbors(),levels = 3)
@@ -213,15 +192,6 @@ knn_preds <- predict(final_wf, new_data=amazonTest, type="prob") %>%
 
 vroom_write(x=knn_preds, file="./KNN_Preds.csv", delim=",")
 
-##PCA
-my_recipe <- recipe(ACTION~., amazonTrain) %>%
-  step_mutate_at(all_numeric_predictors(), fn = factor) %>% # turn all numeric features into factors5
-  step_other(all_nominal_predictors(), threshold = .001) %>% # combines categorical values that occur <5% into an "other" value
-  step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION)) %>%
-  step_normalize(all_predictors()) %>%
-  step_pca(all_predictors(), threshold=.9)
-bake(prep, amazonTest)
-
 ##SVM
 library(kernlab)
 
@@ -233,7 +203,7 @@ svm_wf <- workflow() %>%
   add_recipe(my_recipe) %>%
   add_model(svmRadial)
 
-## Tune smoothness and Laplace here
+## Tune parameters
 tuning_grid <- grid_regular(rbf_sigma(), cost(),levels = 3)
 ## Split data for CV
 folds <- vfold_cv(amazonTrain, v = 5, repeats=1)
